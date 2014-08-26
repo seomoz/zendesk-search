33db7fcc72e550f7fc6300a2a1ed5e7a2117795e
Steffany Pachall <steffany@moz.com>
2014-08-26 13:09:08 -0700

    added ability to add tag to ticket

--- src/app.js
+++ src/app.js
@@ -20,26 +20,37 @@
         },
 
         search: function () {
-            var query = this.$('#search').val();
+            var query = this.$('#search').val().toLowerCase().trim();
             this.getZendesk().search(query, function (results) {
                 console.log(results);
+                $$('#search').val();
             });
         },
 
         add: function () {
-            var tag = this.$('#add-tag').val();
+            var tag = this.$('#add-tag').val().toLowerCase().trim();
+            tag.replace(' ', '_');
+            var ticket = this.ticket();
             this.getZendesk().add(tag, function () {
                 console.log('Added tag ' + tag);
+                $$('#add-tag').val(''); 
             });
+
+            ticket.tags().add(tag);
         },
 
         remove: function () {
-            var tag = this.$('#remove-tag').val();
+            var tag = this.$('#remove-tag').val().toLowerCase().trim();
+            tag.replace(' ', '_');
+            var ticket = this.ticket();
             this.getZendesk().remove(tag, function () {
                 console.log('Removed tag ' + tag);
+                $$('#remove-tag').val('');
             }, function () {
                 console.log('Failed to remove tag ' + tag);
             });
+
+            ticket.tags().remove(tag);
         },
 
         initialize: function() {
