80aea866fb5a256c9fd6c2240a8b04c133679fe7
Steffany Pachall <steffany@moz.com>
2014-08-26 18:56:09 -0700

    changed widget layout

--- app.js
+++ app.js
@@ -3,9 +3,12 @@
     "use strict";
     return {
         events: {
-            'click .addbutton': 'add',
-            'click .searchbutton': 'search',
-            'click .removebutton': 'remove',
+            'click #admin-toggle' : 'adminViewHandler',
+            'click .addbutton': 'addToES',
+            'click .add-tag': 'addToTicket',
+            'click .remove-tag': 'removeFromES',
+            'click .search-add-button': 'searchForAdd',
+            'click .search-remove-button': 'searchForRemove',
             'click .initialize': 'initialize'
         },
 
@@ -19,41 +22,68 @@
             return this.zs;
         },
 
-        search: function () {
+        searchForAdd: function () {
             var me = this,
-                query = this.$('#search').val().toLowerCase().trim();
-            this.getZendesk().search(query, function (results) {
-                console.log(results);
-                me.$('#search').val('');
+                query = this.$('#search-add-tag').val().toLowerCase().trim();
+            this.getZendesk().search(query, function (response) {
+                console.log(response);
+                var results = ['hello', 'world']; // REMOVE: testing fake results. Should "response.results;"
+                results.forEach(function(result) {
+                    var esTag = me.$('<div class="es-tag add-tag">' + result + '</div>');
+                    me.$('#es-add-results').append(esTag);
+                });
+                me.$('#search-add-tag').val('');
             });
         },
 
-        add: function () {
+        searchForRemove: function () {
             var me = this,
-                ticket = this.ticket(),
+                query = this.$('#search-remove-tag').val().toLowerCase().trim();
+            this.getZendesk().search(query, function (response) {
+                console.log(response);
+                var results = ['hello', 'world']; // REMOVE: testing fake results. Should "response.results;"
+                results.forEach(function(result) {
+                    var esTag = me.$('<div class="es-tag remove-tag">' + result + '</div>');
+                    me.$('#es-remove-results').append(esTag);
+                });
+                me.$('#search-remove-tag').val('');
+            });
+        },
+
+        addToTicket: function(event) {
+            var tag = this.$(event.target).html();
+            this.ticket().tags().add(tag);
+            this.$(event.target).remove();
+        },
+
+        addToES: function () {
+            var me = this,
                 tag = this.$('#add-tag').val().toLowerCase().trim();
             tag.replace(' ', '_');
             this.getZendesk().add(tag, function () {
                 console.log('Added tag ' + tag);
                 me.$('#add-tag').val('');
             });
-
-            ticket.tags().add(tag);
         },
 
-        remove: function () {
+        removeFromES: function (event) {
             var me = this,
-                ticket = this.ticket(),
-                tag = this.$('#remove-tag').val().toLowerCase().trim();
-            tag.replace(' ', '_');
+                tag = this.$(event.target).html();
             this.getZendesk().remove(tag, function () {
                 console.log('Removed tag ' + tag);
                 me.$('#remove-tag').val('');
+                me.$(event.target).remove();
             }, function () {
                 console.log('Failed to remove tag ' + tag);
             });
+        },
 
-            ticket.tags().remove(tag);
+        adminViewHandler: function(event) {
+            event.preventDefault();
+            var menuText = this.$("#zendesk-tag-admin-content").is(":visible") ? "admin view" : "close admin";
+            this.$("#admin-toggle").html(menuText);
+            this.$("#zendesk-tag-admin-content").toggle();
+            this.$("#zendesk-tag-widget-content").toggle();
         },
 
         initialize: function () {
